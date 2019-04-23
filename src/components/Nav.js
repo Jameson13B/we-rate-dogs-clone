acbc15f442daab40e6a108e7707db069a0b31c8d
Jameson13B <jamesonbrown@neolivity.com>
2019-04-22 22:20:10 -0600

    Added active styling to links in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../styles/Nav.css';
 
 const Nav = () => {
   return (
     <div className='Nav'>
-      <Link to={'/'}>Explore</Link>
-      <Link to={'/my-list'}>Rated</Link>
+      <NavLink exact to={'/'} activeClassName='active'>
+        Explore
+      </NavLink>
+      <NavLink to={'/my-list'} activeClassName='active'>
+        Rated
+      </NavLink>
     </div>
   );
 };
